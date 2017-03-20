feb2fdec638a64d7d4a9f3b2f5c1a036cf077072
SukaNekoto <purplestripedtiger@gmail.com>
2017-03-19 20:17:21 -0700

    Update 2huZUN_bookmarklet.js

--- 2huZUN_bookmarklet.js
+++ 2huZUN_bookmarklet.js
@@ -2,7 +2,7 @@ javascript:(function(){
 
 //Touhou and ZUN Day Message
 var $touhouCalendar = document.createElement("span");
-$touhouCalendar.append("Today it's Lily White & Black Day! boop");
+$touhouCalendar.append("Today it's Lily White & Black Day!");
 document.getElementsByClassName("player_header")[0].appendChild($touhouCalendar);
 
 
