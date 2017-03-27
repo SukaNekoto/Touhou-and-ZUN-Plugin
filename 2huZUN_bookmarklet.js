aa969c83f0f343faf67a03fd4bbc3f20d46f4865
SukaNekoto <purplestripedtiger@gmail.com>
2017-03-27 06:45:27 -0700

    Update 2huZUN_bookmarklet.js

--- 2huZUN_bookmarklet.js
+++ 2huZUN_bookmarklet.js
@@ -2,7 +2,7 @@ javascript:(function(){
 
 //Touhou and ZUN Day Message
 var $touhouCalendar = document.createElement("span");
-$touhouCalendar.append("Today is Yuyuko Day!");
+$touhouCalendar.append("Mischievous Fairies Day, April 1");
 document.getElementsByClassName("player_header")[0].appendChild($touhouCalendar);
 
 
