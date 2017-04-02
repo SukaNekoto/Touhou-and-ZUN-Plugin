9bebafacfb6c404da3be620f9155cbe61391c37c
SukaNekoto <purplestripedtiger@gmail.com>
2017-04-02 11:17:54 -0700

    Update 2huZUN_bookmarklet.js

--- 2huZUN_bookmarklet.js
+++ 2huZUN_bookmarklet.js
@@ -2,7 +2,7 @@ javascript:(function(){
 
 //Touhou and ZUN Day Message
 var $touhouCalendar = document.createElement("span");
-$touhouCalendar.append("Today's Kana Day!");
+$touhouCalendar.append("Byakuren Day, April 8");
 document.getElementsByClassName("player_header")[0].appendChild($touhouCalendar);
 
 
