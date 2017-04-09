e1393a137d3774296b2b05f59da71ae72715eb3c
SukaNekoto <purplestripedtiger@gmail.com>
2017-04-09 13:12:57 -0700

    Update 2huZUN_bookmarklet.js

--- 2huZUN_bookmarklet.js
+++ 2huZUN_bookmarklet.js
@@ -2,7 +2,7 @@ javascript:(function(){
 
 //Touhou and ZUN Day Message
 var $touhouCalendar = document.createElement("span");
-$touhouCalendar.append("Komachi Day, April 10");
+$touhouCalendar.append("Today's Komachi Day!");
 document.getElementsByClassName("player_header")[0].appendChild($touhouCalendar);
 
 
