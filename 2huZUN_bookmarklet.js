f29adb7328319b42d37e1331a268d0e735843361
SukaNekoto <purplestripedtiger@gmail.com>
2017-04-18 12:21:57 -0700

    Update 2huZUN_bookmarklet.js

--- 2huZUN_bookmarklet.js
+++ 2huZUN_bookmarklet.js
@@ -2,7 +2,7 @@ javascript:(function(){
 
 //Touhou and ZUN Day Message
 var $touhouCalendar = document.createElement("span");
-$touhouCalendar.append("Fan Character Day April 18");
+$touhouCalendar.append("Book Day April 23");
 document.getElementsByClassName("player_header")[0].appendChild($touhouCalendar);
 
 
