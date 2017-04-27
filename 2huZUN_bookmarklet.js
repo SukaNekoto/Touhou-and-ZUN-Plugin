4739b1a38762d73d5279fd11fa572e4939f7a1a4
SukaNekoto <purplestripedtiger@gmail.com>
2017-04-27 09:00:43 -0700

    Update 2huZUN_bookmarklet.js

--- 2huZUN_bookmarklet.js
+++ 2huZUN_bookmarklet.js
@@ -2,7 +2,7 @@ javascript:(function(){
 
 //Touhou and ZUN Day Message
 var $touhouCalendar = document.createElement("span");
-$touhouCalendar.append("Uwabami Breakers Day April 28!");
+$touhouCalendar.append("Today's Uwabami Breakers Day!");
 document.getElementsByClassName("player_header")[0].appendChild($touhouCalendar);
 
 
