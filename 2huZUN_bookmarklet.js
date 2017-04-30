67a72772711a297514659e4af82ef1a223be4aad
SukaNekoto <purplestripedtiger@gmail.com>
2017-04-30 08:49:11 -0700

    Update 2huZUN_bookmarklet.js

--- 2huZUN_bookmarklet.js
+++ 2huZUN_bookmarklet.js
@@ -2,7 +2,7 @@ javascript:(function(){
 
 //Touhou and ZUN Day Message
 var $touhouCalendar = document.createElement("span");
-$touhouCalendar.append("Today's Witches' Day!");
+$touhouCalendar.append("Today's Yuuka Day!");
 document.getElementsByClassName("player_header")[0].appendChild($touhouCalendar);
 
 
