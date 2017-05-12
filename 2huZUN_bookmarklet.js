1452c7d22aa736ae5224d0d19f18fc5bc829b551
SukaNekoto <purplestripedtiger@gmail.com>
2017-05-11 20:33:30 -0700

    Update 2huZUN_bookmarklet.js

--- 2huZUN_bookmarklet.js
+++ 2huZUN_bookmarklet.js
@@ -2,7 +2,7 @@ javascript:(function(){
 
 //Touhou and ZUN Day Message
 var $touhouCalendar = document.createElement("span");
-$touhouCalendar.append("Today's Secret Sealing Day!");
+$touhouCalendar.append("Suwako Day May 13");
 document.getElementsByClassName("player_header")[0].appendChild($touhouCalendar);
 
 
