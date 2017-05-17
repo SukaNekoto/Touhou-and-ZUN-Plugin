b4f9840e1581bb9f68833de6831a7f249677af0d
SukaNekoto <purplestripedtiger@gmail.com>
2017-05-17 08:16:29 -0700

    Update 2huZUN_bookmarklet.js

--- 2huZUN_bookmarklet.js
+++ 2huZUN_bookmarklet.js
@@ -2,7 +2,7 @@ javascript:(function(){
 
 //Touhou and ZUN Day Message
 var $touhouCalendar = document.createElement("span");
-$touhouCalendar.append("Dishes Day May 18!");
+$touhouCalendar.append("Today's Dishes Day!");
 document.getElementsByClassName("player_header")[0].appendChild($touhouCalendar);
 
 
