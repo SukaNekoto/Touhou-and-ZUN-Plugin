caec10a513b56f7ab9dadb1d1465eb0363cf8fc1
SukaNekoto <purplestripedtiger@gmail.com>
2017-05-22 12:19:00 -0700

    Update 2huZUN_bookmarklet.js

--- 2huZUN_bookmarklet.js
+++ 2huZUN_bookmarklet.js
@@ -2,7 +2,7 @@ javascript:(function(){
 
 //Touhou and ZUN Day Message
 var $touhouCalendar = document.createElement("span");
-$touhouCalendar.append("Turtle Day May 23");
+$touhouCalendar.append("Today's Turtle Day!");
 document.getElementsByClassName("player_header")[0].appendChild($touhouCalendar);
 
 
