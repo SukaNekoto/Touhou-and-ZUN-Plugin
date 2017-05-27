17a8efb6b6570bdb46a7825dd9e0a0590e674dd6
SukaNekoto <purplestripedtiger@gmail.com>
2017-05-26 20:37:27 -0700

    Update 2huZUN_bookmarklet.js

--- 2huZUN_bookmarklet.js
+++ 2huZUN_bookmarklet.js
@@ -2,7 +2,7 @@ javascript:(function(){
 
 //Touhou and ZUN Day Message
 var $touhouCalendar = document.createElement("span");
-$touhouCalendar.append("DJ Event Today!");
+$touhouCalendar.append("DJ Event Today UTC 12-13:00!");
 document.getElementsByClassName("player_header")[0].appendChild($touhouCalendar);
 
 
