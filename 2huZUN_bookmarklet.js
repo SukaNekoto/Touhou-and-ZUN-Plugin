0e636389fdb09e73eca8622542131d96f1cb449b
SukaNekoto <purplestripedtiger@gmail.com>
2017-05-29 18:22:13 -0700

    Update 2huZUN_bookmarklet.js

--- 2huZUN_bookmarklet.js
+++ 2huZUN_bookmarklet.js
@@ -2,7 +2,7 @@ javascript:(function(){
 
 //Touhou and ZUN Day Message
 var $touhouCalendar = document.createElement("span");
-$touhouCalendar.append("Kisume Day May 30!");
+$touhouCalendar.append("Today's Kisume Day!");
 document.getElementsByClassName("player_header")[0].appendChild($touhouCalendar);
 
 
