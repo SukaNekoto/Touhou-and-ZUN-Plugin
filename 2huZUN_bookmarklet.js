2647fa939a2d1876096facdbf0da31067268b944
SukaNekoto <purplestripedtiger@gmail.com>
2017-06-07 16:07:06 -0700

    Update 2huZUN_bookmarklet.js

--- 2huZUN_bookmarklet.js
+++ 2huZUN_bookmarklet.js
@@ -2,7 +2,7 @@ javascript:(function(){
 
 //Touhou and ZUN Day Message
 var $touhouCalendar = document.createElement("span");
-$touhouCalendar.append("Sekibanki Day June 6");
+$touhouCalendar.append("Shingyouku Day June 9");
 document.getElementsByClassName("player_header")[0].appendChild($touhouCalendar);
 
 
